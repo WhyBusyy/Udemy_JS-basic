8d3197467496891b08b6161d810047896b64e4b8
WhyBusyy <ybg6152@naver.com>
2023-10-30 23:13:15 +0900

    let -> const // 이후에 변경하지 않는 변수를 상수변수로 수정

--- WEB-DEVELOPMENT/demo.js
+++ WEB-DEVELOPMENT/demo.js
@@ -1,18 +1,18 @@
-let productNameInputElement = document.getElementById('productName');
+const productNameInputElement = document.getElementById('productName');
 // 1. input을 변수로 설정
-let remainingCharsElement = document.getElementById('remainingChars');
+const remainingCharsElement = document.getElementById('remainingChars');
 // 2. 수정되어야할 값을 랩핑하고 있는 span 변수 설정
 
-let maxAllowedChars = productNameInputElement.maxLength;
+const maxAllowedChars = productNameInputElement.maxLength;
 // 7-1. input의 최대길이를 변수로 설정
 
 function updateRemainingCharacters(event) {
 // 4. 발생하는 이벤트를 파라미터로 하는 함수를 만들어줌
-    let enteredText = event.target.value;
+    const enteredText = event.target.value;
     // 5. 발생한 이벤트의 값을 변수로 설정
-    let enteredTextLength = enteredText.length;
+    const enteredTextLength = enteredText.length;
     // 6. 발생한 이벤트의 값.길이를 변수로 설정
-    let remainingCharaters = maxAllowedChars - enteredTextLength;
+    const remainingCharaters = maxAllowedChars - enteredTextLength;
     // 7-2. 최대 길이 - 발생한 이벤트의 값 길이를 변수로 설정
     remainingCharsElement.textContent = remainingCharaters;
     // 8. 수정되어야할 값을 랩핑하고 있는 span.텍스트를 7번에서 만든 변수로 설정
